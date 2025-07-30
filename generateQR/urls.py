9298d3dc97777e859e3f4c899746de75f1fad25e
root <root@srv637262.hstgr.cloud>
2025-07-30 20:03:44 +0000

    bug fixing

--- generateQR/urls.py
+++ generateQR/urls.py
@@ -17,12 +17,22 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.urls import path, include, re_path as url
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.conf.urls.i18n import i18n_patterns
+
+admin.sites.AdminSite.site_header = 'generateQR Admin'
+admin.sites.AdminSite.site_title = 'generateQR'
+admin.sites.AdminSite.index_title = 'generate any QR'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('qr.urls', namespace='qr')),
+    
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += i18n_patterns(path("admin", admin.site.urls))
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
