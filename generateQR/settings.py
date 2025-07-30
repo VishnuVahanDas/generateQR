3a946c9c142a66409ff932e616748308dfb53381
Vipul Kumar <66182594+VishnuVahanDas@users.noreply.github.com>
2025-07-31 02:32:00 +0530

    Update settings.py

--- generateQR/settings.py
+++ generateQR/settings.py
@@ -129,4 +129,4 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Redirect users to the login page after logging out
-LOGOUT_REDIRECT_URL = "login"
+LOGOUT_REDIRECT_URL = "/"
